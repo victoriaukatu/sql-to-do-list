796c1639886d43547c4385010c9c880f63f174ad
Victoria Ukatu <victoria.ukatu@gmail.com>
2019-10-18 16:09:31 -0500

    Set up server.js file to serve back static files in public folder

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+const tasksRouter = require('./routes/task.router.js');
+
+app.use('/tasks', tasksRouter);
+
+// send back static files
+app.use(express.static('server/public'))
 
 
-
+// listen on specified port
 app.listen(PORT, () => {
     console.log('listening on port', PORT);
   });
