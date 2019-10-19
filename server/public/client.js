bada460d2005a5d0194c81a85b6c937f4d933616
Victoria Ukatu <victoria.ukatu@gmail.com>
2019-10-19 12:43:00 -0500

    Test that Add Task button works when clicked

--- server/public/client.js
+++ server/public/client.js
@@ -2,14 +2,18 @@ console.log ('in client.js');
 
 $(document).ready(function(){
     console.log('Ready to go!');
-    $('#addTaskButton').on('click', handleTaskAdd);
+    $('#addTaskButton').on('click', handleTaskSubmit);
 });
 
 
-// function handleTaskAdd() {
+function handleTaskSubmit() {
+    console.log('Add task button has been clicked!');
+}
+
+// function addTask () {
 //     $.ajax({
 //         method: 'POST',
-//         url: ,
+//         url: '/tasks/',
 //         data: {
 //             task:
 //         }
