a138d557c12147c12ff23fb4468baceb60313baf
Victoria Ukatu <victoria.ukatu@gmail.com>
2019-10-19 13:20:01 -0500

    Add function to GET updated task list from the database

--- server/public/client.js
+++ server/public/client.js
@@ -1,6 +1,6 @@
-console.log ('in client.js');
+console.log('in client.js');
 
-$(document).ready(function(){
+$(document).ready(function () {
     console.log('Ready to go!');
     $('#addTaskButton').on('click', handleTaskSubmit);
 });
@@ -13,19 +13,33 @@ function handleTaskSubmit() {
     addTask(collectTask);
 }
 
-function addTask (newTask) {
+function addTask(newTask) {
     $.ajax({
         method: 'POST',
         url: '/tasks/',
-        data: newTask,
+        data: newTask
     })
-    .then(function(response) {
-        console.log('Received response from server:', response);
-        // refreshTaskList();
+        .then(function (response) {
+            console.log('Received response from server:', response);
+            refreshTaskList();
+        })
+        .catch((error) => {
+            console.log('Error in POST', error);
+            alert('Your new task did not add correctly. Please try again.');
+            res.sendStatus(500);
+        });
+}
+
+function refreshTaskList() {
+    $.ajax({
+        method: 'GET',
+        url: '/tasks'
     })
-    .catch((error) => {
-        console.log('Error in POST', error);
-        alert('Your new task did not add correctly. Please try again.');
-        res.sendStatus(500);
-    });
+        .then(function (response) {
+            console.log('Received from server:', response);
+            appendTaskList(response);
+        })
+        .catch((error) => {
+            console.log('Error receiving updated task list', error);
+        });
 }
